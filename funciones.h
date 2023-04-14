b294cfc79c8e7b719a7c65833285cefaed6d8b17
Álvaro <alvarogarciavelasco12@gmail.com>
2023-04-14 10:30:22 +0200

    Definicion tipos de datos tipoContacto y tipoAgenda

--- funciones.h
+++ funciones.h
@@ -4,5 +4,20 @@
 
 #ifndef PRACTICA4_FUNCIONES_H
 #define PRACTICA4_FUNCIONES_H
+#define MAX 250
+
+typedef enum CONTACTO{
+    FAVORTO, CONOCIDO, TRABAJO
+}tContacto;
+
+typedef struct AGENDA{
+    char nombre[MAX];
+    char apellido[MAX];
+    char num[MAX];
+    tContacto tipoContacto
+}tAgenda;
+
+
+
 
 #endif //PRACTICA4_FUNCIONES_H
